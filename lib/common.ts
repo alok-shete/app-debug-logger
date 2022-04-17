4da725ff9fd8afa5e815585229188c9da6474a42
Alok Shete <alok.shete@vendekin.com>
2022-04-17 16:29:35 +0530

    marge master

--- lib/common.ts
+++ lib/common.ts
@@ -1,39 +1,40 @@
-const https = require("https");
-const loggerCurrentVersion = require("./../package.json").version;
-const packageJsonPath = require("./../package.json").repository.path;
+// const https = require('https');
+const loggerCurrentVersion = require('./../package.json').version;
+// const packageJsonPath = require('./../package.json').repository.path;
 
 /**
- *
+ * get package json
  * @returns
  */
-const getPackagesJson = function () {
-  let options = {
-    host: "api.github.com",
-    path: packageJsonPath,
-    method: "GET",
-    headers: { "user-agent": "node.js" },
-  };
-  return new Promise(function (resolve, reject) {
-    let request = https.request(options, function (response: any) {
-      let body = "";
-      response.on("data", function (chunk: any) {
-        body += chunk.toString("utf8");
-      });
-      response.on("end", function () {
-        try {
-          let content = JSON.parse(body).content;
-          let buf = Buffer.from(content, "base64");
-          let packageJson = JSON.parse(buf.toString());
-          resolve(packageJson);
-        } catch (err) {
-          reject(err);
-        }
-      });
-    });
-    request.end();
-    request.on("error", function (err: any) {
-      reject(err);
-    });
+const getPackagesJson = function() {
+  // let options = {
+  //   host: 'api.github.com',
+  //   path: packageJsonPath,
+  //   method: 'GET',
+  //   headers: { 'user-agent': 'node.js' },
+  // };
+  return new Promise(function(resolve) {
+    // let request = https.request(options, function(response: any) {
+    //   let body = '';
+    //   response.on('data', function(chunk: any) {
+    //     body += chunk.toString('utf8');
+    //   });
+    //   response.on('end', function() {
+    //     try {
+    //       let content = JSON.parse(body).content;
+    //       let buf = Buffer.from(content, 'base64');
+    //       let packageJson = JSON.parse(buf.toString());
+    //       resolve(packageJson);
+    //     } catch (err) {
+    //       reject(err);
+    //     }
+    //   });
+    // });
+    // request.end();
+    // request.on('error', function(err: any) {
+    //   reject(err);
+    // });
+    resolve({});
   });
 };
 /**
@@ -43,7 +44,7 @@ const getPackagesJson = function () {
  */
 const parseDate = (fullDate: Date) => {
   const d = new Date(fullDate);
-  const date = d.toISOString().split("T")[0];
+  const date = d.toISOString().split('T')[0];
   const hour = `0${d.getHours()}`.slice(-2);
   const min = `0${d.getMinutes()}`.slice(-2);
   const sec = `0${d.getSeconds()}`.slice(-2);
@@ -61,10 +62,10 @@ const printConsoleLog = (payload: any) => {
    * log text color content
    */
   const logColor: any = {
-    info: "\x1b[36m%s\x1b[0m",
-    success: "\x1b[32m%s\x1b[0m",
-    error: "\x1b[31m%s\x1b[0m",
-    warn: "\x1b[33m%s\x1b[0m",
+    info: '\x1b[36m%s\x1b[0m',
+    success: '\x1b[32m%s\x1b[0m',
+    error: '\x1b[31m%s\x1b[0m',
+    warn: '\x1b[33m%s\x1b[0m',
   };
 
   /**
@@ -86,8 +87,8 @@ const printConsoleLog = (payload: any) => {
 
 const versionToNumber = (version: string) => {
   const versionArr =
-    version.split(".").length === 3 ? version.split(".") : ["0", "0", "0"];
-  let [major, minor, patch] = versionArr.map((versionNumber) => {
+    version.split('.').length === 3 ? version.split('.') : ['0', '0', '0'];
+  let [major, minor, patch] = versionArr.map(versionNumber => {
     return `0000${versionNumber}`;
   });
 
@@ -98,9 +99,46 @@ const versionToNumber = (version: string) => {
   return Number(numVersion) || 0;
 };
 
+const validateLoggerConfig = (config: {
+  id?: 'logger_event' | undefined;
+  isEventLog?: false | undefined;
+  consoleLog?: string[] | undefined;
+  isProduction?: false | undefined;
+}) => {
+  const {
+    id = 'logger_event',
+    isEventLog = false,
+    consoleLog = ['info', 'error', 'warn', 'success', 'debug'],
+    isProduction = false,
+  } = config;
+
+  const validConfig: {
+    id?: 'logger_event';
+    isEventLog?: false;
+    consoleLog?: string[];
+    isProduction?: false;
+  } = config;
+
+  if (typeof id !== 'string') {
+    validConfig.id = 'logger_event';
+  }
+  if (typeof isEventLog !== 'boolean') {
+    validConfig.isEventLog = false;
+  }
+  if (typeof isProduction !== 'boolean') {
+    validConfig.isProduction = false;
+  }
+  if (!Array.isArray(consoleLog)) {
+    validConfig.consoleLog = ['info', 'error', 'warn', 'success', 'debug'];
+  }
+
+  return validConfig;
+};
+
 export default {
   getPackagesJson: getPackagesJson,
   printConsoleLog: printConsoleLog,
   loggerCurrentVersion: loggerCurrentVersion,
   versionToNumber: versionToNumber,
+  validateLoggerConfig: validateLoggerConfig,
 };
--- lib/index.ts
+++ lib/index.ts
@@ -1,47 +1,94 @@
-import * as EventEmitter from "events";
-import common from "./common";
+import { EventEmitter } from 'events';
+import common from './common';
 
 const {
   printConsoleLog,
   loggerCurrentVersion,
   getPackagesJson,
   versionToNumber,
+  validateLoggerConfig,
 } = common;
 
-export default class Logger extends EventEmitter {
-  id: any;
-  constructor(config: any) {
+// interface LoggerConfig {
+//   id: string;
+//   eventLog: any;
+//   consoleLog: any;
+//   isProduction: boolean;
+// }
+
+export class Logger extends EventEmitter {
+  id: string;
+  isEventLog?: false;
+  consoleLog?: string[];
+  isProduction?: false;
+  debug: Function;
+  info: Function;
+  error: Function;
+  warn: Function;
+  success: Function;
+
+  constructor(config: {
+    id?: 'logger_event';
+    isEventLog?: false;
+    consoleLog?: string[];
+    isProduction?: false;
+  }) {
     super();
-    const { id = "logger_event" } = config;
+    const {
+      id = 'logger_event',
+      isEventLog = false,
+      consoleLog = ['info', 'error', 'warn', 'success', 'debug'],
+      isProduction = false,
+    } = validateLoggerConfig(config);
     this.id = id;
+    this.isEventLog = isEventLog;
+    this.consoleLog = consoleLog;
+    this.isProduction = isProduction;
 
-    console.log("*** Welcome in App Logger ***");
-    console.log("*** Current Version : ", loggerCurrentVersion);
+    console.log('*** Welcome in App Logger ***');
+    console.log('*** Current Version : ', loggerCurrentVersion);
+    this.checkLoggerVersion();
 
-    let loggerLiveVersion = "0.0.0";
+    this.debug = (moduleName: string, message: string) => {
+      this.processLog('debug', moduleName, message);
+    };
+    this.info = (moduleName: string, message: string) => {
+      this.processLog('info', moduleName, message);
+    };
+    this.error = (moduleName: string, message: string) => {
+      this.processLog('error', moduleName, message);
+    };
+    this.warn = (moduleName: string, message: string) => {
+      this.processLog('warn', moduleName, message);
+    };
+    this.success = (moduleName: string, message: string) => {
+      this.processLog('success', moduleName, message);
+    };
+  }
+
+  /**
+   * check Logger version
+   */
+  private checkLoggerVersion() {
+    let loggerLiveVersion = '0.0.0';
     getPackagesJson()
       .then((res: any) => {
-        const packageJsonVersion = res.version ?? "0.0.0";
+        const packageJsonVersion = res.version ?? '0.0.0';
         loggerLiveVersion = packageJsonVersion;
-
-        console.log(loggerLiveVersion);
 
         if (
           versionToNumber(loggerLiveVersion) >
           versionToNumber(loggerCurrentVersion)
         ) {
           this.processLog(
-            "warn",
-            "LOGGER",
-            "*** Seems you are using old package please update ***",
+            'warn',
+            'LOGGER',
+            '*** Seems you are using old package please update ***',
             false
           );
         }
       })
-      .catch((err) => {
-        console.log(err);
-        console.log("err");
-      });
+      .catch(() => {});
   }
 
   /**
@@ -50,7 +97,12 @@ export default class Logger extends EventEmitter {
    * @param {Object} payload
    */
   private createEvent(event: string, payload: any) {
+    if (!this.isEventLog) {
+      return true;
+    }
     this.emit(this.id, { event, payload });
+
+    return true;
   }
 
   /**
@@ -66,6 +118,9 @@ export default class Logger extends EventEmitter {
     message: string,
     isEventEmit = true
   ) {
+    if (!this.consoleLog?.includes(type)) {
+      return true;
+    }
     const payload = {
       type,
       loggedAt: new Date(),
@@ -73,52 +128,13 @@ export default class Logger extends EventEmitter {
       message,
     };
 
-    printConsoleLog(payload);
-    isEventEmit ? this.createEvent("log", payload) : null;
-  }
+    if (!this.isProduction) {
+      printConsoleLog(payload);
+    }
+    if (isEventEmit) {
+      this.createEvent('log', payload);
+    }
 
-  /**
-   *
-   * @param {string} moduleName
-   * @param {string} message
-   */
-  debug(moduleName: string, message: string) {
-    this.processLog("debug", moduleName, message);
-  }
-
-  /**
-   *
-   * @param {string} moduleName
-   * @param {string} message
-   */
-  info(moduleName: string, message: string) {
-    this.processLog("info", moduleName, message);
-  }
-  /**
-   *
-   * @param {string} moduleName
-   * @param {string} message
-   */
-
-  success(moduleName: string, message: string) {
-    this.processLog("success", moduleName, message);
-  }
-
-  /**
-   *
-   * @param {string} moduleName
-   * @param {string} message
-   */
-  error(moduleName: string, message: string) {
-    this.processLog("error", moduleName, message);
-  }
-
-  /**
-   *
-   * @param {string} moduleName
-   * @param {string} message
-   */
-  warn(moduleName: string, message: string) {
-    this.processLog("warn", moduleName, message);
+    return true;
   }
 }
